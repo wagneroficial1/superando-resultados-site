00bc6c9dcc5998875f0e880718ad260e41b99b47
cashfy <wagneroficial@gmail.com>
2026-06-11 14:36:37 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL?.trim()
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY?.trim()
+
+console.log('SUPABASE URL:', supabaseUrl)
 
 if (!supabaseUrl || !supabaseAnonKey) {
+  alert('Erro: variáveis do Supabase não carregaram.')
   throw new Error('Variáveis do Supabase não configuradas.')
 }
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
